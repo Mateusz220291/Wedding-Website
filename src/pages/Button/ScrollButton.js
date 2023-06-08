664de667c971a367db19a9277357c4869bd06cf1
Mateusz <mateuszmikla@wp.pl>
2023-06-08 11:38:01 +0200

    Updates

--- src/pages/Button/ScrollButton.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./ScrollButton.css";
-
-export const ScrollDownBTN = ({ handleClick }) => {
-  return <button className="scroll-down" onClick={handleClick}></button>;
-};
